fbae508339b7db6570ab2febb0e7c54904131779
polinausova <polyusova22@gmail.com>
2020-06-03 23:59:30 +0500

    Fundamentals

--- general.py
+++ general.py
@@ -1,8 +1,24 @@
-def min_value(data):
-    m = data[0]
-    i = 1
-    while i < len(data):
-        if data[i] < m:
-            m = data[i]
-        i += 1
-    return m
+import telebot
+import config
+
+from telebot import apihelper
+bot = telebot.TeleBot(config.API_TOKEN)
+apihelper.proxy = {'https': 'socks5://54.38.195.161:45117'}
+with open('database.txt', 'r', encoding='utf-8') as f:
+    data = f.read().splitlines()
+
+
+@bot.message_handler(commands=['start'])  # command to begin
+def start_message(message):
+    bot.send_message(message.chat.id, "Hi, let's be healthy and safe together!")  # Answer to the message
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text in data:
+        bot.send_message(message.chat.id, 'Yeah, we have this in the list')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
+
+bot.polling()
