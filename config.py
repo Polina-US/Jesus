3e1d4e266b7752889011980217c338ec08c8cbe0
polinausova <polyusova22@gmail.com>
2020-06-06 02:11:24 +0500

    Upgrade

--- config.py
+++ config.py
@@ -1,1 +1,60 @@
 API_TOKEN = '1088294101:AAEGUbRuAEoxSfT1NrHYxzRghlqu5zVfPiU'
+
+
+class Ingredient:
+    def __init__(self, name, description, danger):
+        self.name = name
+        self.description = description
+        self.danger = danger
+
+
+def database_info():
+    with open('database.txt') as f:
+        list_of_all = []
+        for line in f:
+            line = line.strip().split(';')
+            name = line[0]
+            description = line[1]
+            danger = line[2]
+            list_of_all.append(Ingredient(name, description, danger))
+    return list_of_all
+
+
+def read_message():
+        ing = message.text.strip().split(",")
+        message_list = ing
+        return message_list
+
+
+def View_ingredients(message_ans, classes_list, bot, where_send):
+    list_of_absent = []
+    list_of_bad = []
+    for i in message_ans:
+        for r in classes_list:
+            if i == r.name:
+                if r.danger == "-":
+                    list_of_bad.append(r.name)
+            else:
+                list_of_absent.append(i)
+    if len(list_of_bad) > 0 and len(list_of_absent) > 0:
+        bot.send_message(where_send, "Oops! These ingredients may be dangerous for you:", list_of_bad)
+        bot.send_message(where_send, "Sorry! These elements are not found in our database:", list_of_absent)
+    elif len(list_of_bad) > 0 and len(list_of_absent) == 0:
+        bot.send_message(where_send, "Oops! These ingredients may be dangerous for you:", list_of_bad)
+    elif len(list_of_bad) == 0 and len(list_of_absent) > 0:
+        bot.send_message(where_send, "Everything is safe!")
+        bot.send_message(where_send, "Sorry! These elements are not found in our database:", list_of_absent)
+    elif len(list_of_bad) == 0 and len(list_of_absent) > 0:
+        bot.send_message(where_send, "Everything is safe!")
+
+
+def show_description():
+    messageis = False
+    for i in list_of_all:
+        if i.name == message.text:
+            messageis = True
+            bot.send_message(message.chat.id,"Name: ", i.name, "description: ", i.description)  # make them work for bot
+        elif messageis == False:
+            bot.send_message(message.chat.id, "Sorry! We do not have this ingredient in the database yet.")  # make work for bot
+
+
--- general.py
+++ general.py
@@ -1,41 +1,56 @@
 import telebot
 import config
-
-from telebot import apihelper
+from config import database_info
+from config import View_ingredients
+from config import read_message
+from config import show_description
 
 bot = telebot.TeleBot(config.API_TOKEN)
-apihelper.proxy = {'https': 'socks5://54.38.195.161:45117'}
-with open('database.txt', 'r', encoding='utf-8') as f:
-    data = f.read().splitlines()
+list_of_all = database_info()
 
-database_info()
 
 @bot.message_handler(commands=['start'])  # command to begin, try to switch line 12 with line 13 to make the fuction work while put in any place
 def start_message(message):
-    bot.send_message(message.chat.id, "Hi, let's be healthy and safe together!""\n" # Answer to the message
-                     "If you want to check whether your products contains bad ingredients, enter 1""\n"   # 1 option to see whether a list of ingredients contain a "bad" ones (the ingredients with comma(,) after each)
-                     "If you want to know more about an ingredient enter 2""\n"
-                     "If you want to say goodbye than enter'Bye'")  # 2 option to see its description name
+    bot.send_message(message.chat.id, "Hi, let's be healthy and safe together!""\n"  # Answer to the message
+                                      "If you want to check whether your products contain bad ingredients, enter /1""\n"  # 1 option to see whether a list of ingredients contain a "bad" ones (the ingredients with comma(,) after each)
+                                      "If you want to know more about an ingredient enter /2""\n"
+                                      "If you want to say goodbye than enter'Bye'")  # 2 option to see its description name
 
+@bot.message_handler(commands=['1'])
+def finding_danger(message):
+    to_whom = message.chat.id
+    bot.send_message(message.chat.id, 'Enter the ingredients with comma(,) after each')
+    ing = message.text.strip().split(",")
+    message_list = ing
+    View_ingredients(message_list, list_of_all, bot, to_whom)
+
+
+@bot.message_handler(commands=['2'])
+def know_more(message):
+    bot.send_message(message.chat.id, 'Enter the name of an ingredient')
+    show_description()
 
 @bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text == 1:#code for option 1
-        bot.send_message(message.chat.id, 'Enter the ingredients with comma(,) after each')
-        @bot.message_handler(content_types=['text'])#not sure
-        read_message()
-        View_ingredients()
-        start_message() #not sure
-    elif message.text == 2:#code for option 2
-        show_description()
-        start_message() #not sure
-    elif message.text == 'Bye':
-        bot.send_message(message.chat.id, 'Goodbye, creator!')
-    else:
-        bot.send_message(message.chat.id, 'You have entered the message I cannot understand. try again!')
-        start_message()
+def some_issues(message):
+    bot.send_message(message.chat.id, 'You have entered the message I cannot understand. try again!')
 
 
+# def send_text(message):
+#     to_whom = message.chat.id
+#     if message.text == '1':  # code for option 1
+#         bot.send_message(message.chat.id, 'Enter the ingredients with comma(,) after each')
+#         @bot.message_handler(content_types=['text'])
+#         d
+#         ing = message.text.strip().split(",")
+#         message_list = ing
+#         View_ingredients(message_list, list_of_all, bot, to_whom)
+#     elif message.text == '2':
+#         bot.send_message(message.chat.id, 'Enter the name of an ingredient')
+#         show_description()
+#     elif message.text == 'Bye'.lower():
+#         bot.send_message(message.chat.id, 'Goodbye!')
+#     else:
+#         bot.send_message(message.chat.id, 'You have entered the message I cannot understand. try again!')
 
 
 
